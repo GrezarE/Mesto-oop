6b84dfe5b71a91d633346868cc84827dddd18efa
Kettrin62 <KettyKazakova@yandex.ru>
2021-08-21 20:36:59 +0300

    открытие/закрытие

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const infoButton = bodyElement.querySelector('.info__button');
 //находим модальное окно редактирования
 const popupInfoElement = bodyElement.querySelector('.popup_option_info');
 //находим кнопку, по которой модальное окно закрывается
-const closeButton = bodyElement.querySelector('.popup__close-icon');
+const closeButtonInfo = bodyElement.querySelector('section.popup_option_info .popup__close-icon');
 
 //находим кнопку, по которой открывается модальное окно добавления
 const addButton = bodyElement.querySelector('.profile__button-add');
 //находим модальное окно добавления
 const popupAddElement = bodyElement.querySelector('.popup_option_add');
+//находим кнопку, по которой модальное окно закрывается
+const closeButtonAdd = bodyElement.querySelector('section.popup_option_add .popup__close-icon');
 
 
 //добавляем обработчик клика по кнопке "редактировать"
@@ -19,7 +21,7 @@ infoButton.addEventListener('click', () => {
 });
 
 //добавляем обработчик клика по кнопке "закрыть"
-popupInfoElement.addEventListener('click', () => {
+closeButtonInfo.addEventListener('click', () => {
   popupInfoElement.classList.remove('popup_opened');
 });
 
@@ -30,6 +32,8 @@ addButton.addEventListener('click', () => {
 });
 
 //добавляем обработчик клика по кнопке "закрыть"
-popupAddElement.addEventListener('click', () => {
+closeButtonAdd.addEventListener('click', () => {
   popupAddElement.classList.remove('popup_opened');
 });
+
+

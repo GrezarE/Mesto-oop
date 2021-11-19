82e6d46198f06be2186882551108600b9f7a8e9e
Grechishnikov-Zarin Evgeniy <grecha-zarin@yandex.ru>
2021-11-19 21:56:55 +0300

    добавляем класс валидации

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,31 @@
-import './index.css'; // добавьте импорт главного файла стилей
+import "./index.css"; // добавьте импорт главного файла стилей
 
-import { popupInfoElement } from '../components/utilits.js';
-import { popupAddElement } from '../components/utilits.js';
-import { popups } from '../components/utilits.js';
-import { popupAvatarElement } from '../components/utilits.js';
-import { formAddElement } from '../components/utilits.js';
-import { nameText, jobText, avatarLinkText } from '../components/utilits.js';
-import { formEditElement } from '../components/utilits.js';
-import { nameInput, jobInput } from '../components/utilits.js';
-import { infoButton } from '../components/utilits.js';
-import { addButton } from '../components/utilits.js';
-import { formAvatarElement } from '../components/utilits.js';
-import { avatarButton } from '../components/utilits.js';
-import { openPopup } from '../components/utilits.js';
-import { cardsContainer } from '../components/utilits.js';
-import { closePopupButtonOverlay } from '../components/modal.js';
-import { formSubmitEditHandler } from '../components/modal.js';
-import { formSubmitAddHandler } from '../components/modal.js';
-import { formSubmitUpdateAvatarHandler } from '../components/modal.js';
-import { enableValidation } from '../components/validate.js';
-import { addCard } from '../components/card.js';
+import { popupInfoElement } from "../components/utilits.js";
+import { popupAddElement } from "../components/utilits.js";
+import { popups } from "../components/utilits.js";
+import { popupAvatarElement } from "../components/utilits.js";
+import { formAddElement } from "../components/utilits.js";
+import { nameText, jobText, avatarLinkText } from "../components/utilits.js";
+import { formEditElement } from "../components/utilits.js";
+import { nameInput, jobInput } from "../components/utilits.js";
+import { infoButton } from "../components/utilits.js";
+import { addButton } from "../components/utilits.js";
+import { formAvatarElement } from "../components/utilits.js";
+import { avatarButton } from "../components/utilits.js";
+import { openPopup } from "../components/utilits.js";
+import { cardsContainer } from "../components/utilits.js";
+import { closePopupButtonOverlay } from "../components/modal.js";
+import { formSubmitEditHandler } from "../components/modal.js";
+import { formSubmitAddHandler } from "../components/modal.js";
+import { formSubmitUpdateAvatarHandler } from "../components/modal.js";
+// import { enableValidation } from "../components/validate.js";
+import { addCard } from "../components/card.js";
 
+import { configValid } from "../utils/constants.js";
+import FormValidator from "../components/FormValidator";
 
 // добавляем обработчик клика по кнопке "редактировать"
-infoButton.addEventListener('click', () => {
+infoButton.addEventListener("click", () => {
   openPopup(popupInfoElement);
   // вставим начальные данные из профиля в поля формы
   nameInput.value = nameText.textContent;
@@ -31,43 +33,50 @@ infoButton.addEventListener('click', () => {
 });
 
 // добавляем обработчик клика по кнопке "добавить"
-addButton.addEventListener('click', () => {
+addButton.addEventListener("click", () => {
   openPopup(popupAddElement);
 });
 
 // добавляем обработчик клика по кнопке "аватар"
-avatarButton.addEventListener('click', () => {
+avatarButton.addEventListener("click", () => {
   openPopup(popupAvatarElement);
 });
 
 popups.forEach((popup) => {
-    popup.addEventListener('click', (evt) => {
-      closePopupButtonOverlay(popup, evt);
-    })
-})
-
-// Прикрепляем обработчик к форме редактирования: он будет следить за событием “submit” - «отправка»
-formEditElement.addEventListener('submit', formSubmitEditHandler);
-
-// Прикрепляем обработчик к форме добавления карточек: он будет следить за событием “submit” - «отправка»
-formAddElement.addEventListener('submit', formSubmitAddHandler);
-
-// Прикрепляем обработчик к форме обновления аватара: он будет следить за событием “submit” - «отправка»
-formAvatarElement.addEventListener('submit', formSubmitUpdateAvatarHandler);
-
-enableValidation({
-  formSelector: '.form-edit',
-  inputSelector: '.form-edit__item',
-  submitButtonSelector: '.form-edit__button-save',
-  inputErrorClass: 'form-edit__item_error',
+  popup.addEventListener("click", (evt) => {
+    closePopupButtonOverlay(popup, evt);
+  });
 });
 
+// Прикрепляем обработчик к форме редактирования: он будет следить за событием “submit” - «отправка»
+formEditElement.addEventListener("submit", formSubmitEditHandler);
+
+// Прикрепляем обработчик к форме добавления карточек: он будет следить за событием “submit” - «отправка»
+formAddElement.addEventListener("submit", formSubmitAddHandler);
+
+// Прикрепляем обработчик к форме обновления аватара: он будет следить за событием “submit” - «отправка»
+formAvatarElement.addEventListener("submit", formSubmitUpdateAvatarHandler);
+
+// enableValidation({
+//   formSelector: ".form-edit",
+//   inputSelector: ".form-edit__item",
+//   submitButtonSelector: ".form-edit__button-save",
+//   inputErrorClass: "form-edit__item_error",
+// });
+
+// Находим массив всех форм
+const formList = Array.from(document.querySelectorAll(".form-edit"));
+
+// Для каждой формы создаем экземпляр класса
+formList.forEach((form) => {
+  const validation = new FormValidator({data: configValid}, form);
+  validation.enableValidation();
+});
 
 export let userId;
 
-import { getUserInfo } from '../components/api.js';
-import { getInitialCards } from '../components/api.js';
-
+import { getUserInfo } from "../components/api.js";
+import { getInitialCards } from "../components/api.js";
 
 Promise.all([getUserInfo(), getInitialCards()])
   .then((result) => {
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+// Объект селекторов для валидации
+export const configValid = {
+  inputSelector: ".form-edit__item",
+  submitButtonSelector: ".form-edit__button-save",
+  inputErrorClass: ".form-edit__item_error",
+};
